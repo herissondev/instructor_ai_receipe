9027ae08e576f40283f606d5dc79db933255544e
Risson <aime.risson.1@gmail.com>
2024-03-26 22:18:09 +0100

    change steps - ingredients order

--- lib/ai_recipe/recipes/receipe.ex
+++ lib/ai_recipe/recipes/receipe.ex
@@ -11,15 +11,15 @@ defmodule AiRecipe.Recipe do
   - name: The name of the recipe, providing a brief and catchy description of the dish.
   - total_estimated_duration: The total estimated time required to prepare and cook the dish, measured in minutes.
   - total_estimated_price: An approximate cost to prepare the dish, intended as a guideline to help users gauge the expense.
-  - ingredients: A comprehensive list of all ingredients needed for the recipe, embedded as `Ingredient` structures. This includes quantities, preparation notes, and any specific variants of ingredients.
   - steps: A detailed sequence of actions or instructions to follow for preparing the dish, embedded as `Step` structures. This covers everything from initial preparation to final serving suggestions, ensuring clarity and ease of follow-through for the user.
+  - ingredients: A comprehensive list of all ingredients needed for the recipe, embedded as `Ingredient` structures. This includes quantities, preparation notes, and any specific variants of ingredients.
   """
   @primary_key false
   embedded_schema do
     field(:name, :string)
     field(:total_estimated_duration, :integer)
     field(:total_estimated_price, :float)
-    embeds_many(:ingredients, Ingredient)
     embeds_many(:steps, Step)
+    embeds_many(:ingredients, Ingredient)
   end
 end
